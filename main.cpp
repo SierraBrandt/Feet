b2d4726f46a43bcc77ae4764d99256ef50f27cb0
Sierra Brandt <44011600+SierraBrandt@users.noreply.github.com>
2021-09-08 16:44:48 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//Creates photo of feet
+//Author: Sierra Brandt
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "oOOO0" << endl ;
+  cout << "(   ) 0OOOo "<< endl ;
+  cout << " \\ (  (   ) " << endl ;
+  cout << "  \\_)  ) /"<< endl ;
+  cout << "      (_/  "<< endl ;
+}
